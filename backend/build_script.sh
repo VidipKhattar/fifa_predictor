fd275c08f0a65e22b687e09a0111d061d380831b
VidipKhattar <vidipkhattar13@gmail.com>
2024-07-19 02:08:21 +0530

    pplsd

--- backend/build_script.sh
+++ backend/build_script.sh
@@ -1,10 +1,10 @@
-# backend/build_script.sh
-
 #!/bin/sh
 
 echo "BUILD START"
 python -m pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput --clear
-echo "END START"
+echo "BUILD END"
 
+# Run Django development server
+daphne -b 0.0.0.0 -p 8000 fifa_predictor.asgi:application
